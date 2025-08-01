7ad7c311f7d67da035909ecbe52b114a0a117754
Krishna Praveen <sureshradha084@gmai.com>
2025-08-01 15:14:38 +0530

    4nd commit

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['allure-playwright']],
+   reporter: [
+    ['list'], // shows output in terminal
+    ['allure-playwright'],
+    ['html'] // generates allure-results/
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
